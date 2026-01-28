3727424b4276e13aba7ed2a74fb5f31b126713cf
JairoHervert <158542532+JairoHervert@users.noreply.github.com>
2026-01-27 23:53:51 -0600

    Add readme

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hervert-fedora/Escritorio/Orca_Application_Tier/src/infrastructure/config/ConfigEnv.cpp" "CMakeFiles/main.dir/src/infrastructure/config/ConfigEnv.cpp.o" "gcc" "CMakeFiles/main.dir/src/infrastructure/config/ConfigEnv.cpp.o.d"
+  "/home/hervert-fedora/Escritorio/Orca_Application_Tier/src/interfaces/HttpApi.cpp" "CMakeFiles/main.dir/src/interfaces/HttpApi.cpp.o" "gcc" "CMakeFiles/main.dir/src/interfaces/HttpApi.cpp.o.d"
+  "/home/hervert-fedora/Escritorio/Orca_Application_Tier/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "" "main" "gcc" "CMakeFiles/main.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/link.d
@@ -0,0 +1,139 @@
+main: \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o \
+  CMakeFiles/main.dir/src/main.cpp.o \
+  CMakeFiles/main.dir/src/infrastructure/config/ConfigEnv.cpp.o \
+  CMakeFiles/main.dir/src/interfaces/HttpApi.cpp.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libcryptopp.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libssl.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libcrypto.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_core.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_mysql.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libmariadb.so \
+  /usr/lib64/libssl.so \
+  /usr/lib64/libcrypto.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_core.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_mysql.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libmariadb.so \
+  /usr/lib64/libmariadb.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtend.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o \
+  /usr/lib64/libz.so.1 \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o:
+
+CMakeFiles/main.dir/src/main.cpp.o:
+
+CMakeFiles/main.dir/src/infrastructure/config/ConfigEnv.cpp.o:
+
+CMakeFiles/main.dir/src/interfaces/HttpApi.cpp.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libcryptopp.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libssl.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libcrypto.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_core.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_mysql.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libmariadb.so:
+
+/usr/lib64/libssl.so:
+
+/usr/lib64/libcrypto.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_core.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libsoci_mysql.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libmariadb.so:
+
+/usr/lib64/libmariadb.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtend.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o:
+
+/usr/lib64/libz.so.1:
+
+/lib64/ld-linux-x86-64.so.2:
